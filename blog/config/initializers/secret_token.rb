0c2f41cd88603148d35ccd299b88272e0a339261
Andres Vicente <andres.vicente@globant.com>
2014-02-12 09:29:18 -0500

    Ruby on Rails, getting started - 6.3 Adding a Route for Comments

---
+++ blog/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = 'cd51841b8410649fe72b9f7c12e758909869c8733befcfb2ecec88b1bf9cab09afd39c81eea5b8910f6f42f4ae33a359af541a38a22ab0e974fccc6e729fb20b'
